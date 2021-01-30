cd4126941a69cd7b7445dfdfa7089107584e140f
Kirill Feschenko <swipduces@yandex.com>
2021-01-30 17:01:31 +0300

    Photo recognition.

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from ruotvet.otvetmail import OtvetMail
 from ruotvet.znanija import Znanija
 
+from ruotvet.utils import Recognizer
+
 zn = Znanija()
 otvet = OtvetMail()
 
 
-question = "гомологу этиленгликоля соотвецтвует формула"
+question = " ".join(Recognizer().get_text_from_image("ruotvet/utils/img.png", threshold=True).split()[:8])
+
+print(question)
 
 print(
-    zn.get_answers(question, count=1), "Znanija.com"
+    zn.get_answers(question, count=3), "Znanija.com"
 )
-
+#
 print(
-    otvet.get_answers(question, count=1), "otvet.mail.ru"
+    otvet.get_answers(question, count=3), "otvet.mail.ru"
 )
