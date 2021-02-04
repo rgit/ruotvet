1ee251b58c985ebbb224ce8989a6693f65da7bf0
Kirill Feschenko <swipduces@yandex.com>
2021-02-04 18:03:09 +0300

    Made the repository a package. Transferred a bot to another repo.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+try:
+    with open("README.md", "r") as readme:
+        long_description = readme.read()
+except:
+    long_description = "Library for searching answers to school questions."
+
+setup(
+    name="ruotvet",
+    version="0.0.1",
+    description="Library for searching answers to school questions.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="xcaq",
+    author_email="swipduces@yandex.com",
+    python_requires=">=3.6.0",
+    url="https://github.com/ruotvet/ruotvet",
+    packages=find_packages(),
+    install_requires=["requests", "beautifulsoup4", "opencv-python", "easyocr", "numpy"],
+    include_package_data=True,
+    license="GNU LGPLv3",
+    classifiers=[
+        "License :: OSI Approved :: GNU LGPLv3 License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6+",
+    ]
+)
