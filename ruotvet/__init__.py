dcae499455e64caff896da358ded1b35c852c120
Kirill Feschenko <swipduces@yandex.com>
2021-02-26 00:08:54 +0300

    Fixed paths flaw.

--- ruotvet/__init__.py
+++ ruotvet/__init__.py
@@ -3,6 +3,6 @@ if sys.version_info < (3, 7):
     raise ImportError(f"Your Python version {'.'.join(map(str, sys.version_info[:3]))} is not supported by RuOtvet, "
                       "please install Python 3.7+")
 
-from .clients import *
+from .clients import Brainly, SuperResheba, YandexQ, GDZ, MailRu
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
