8df903dedf8aed01428a6d527f0abee19e2bd191
aiexz <aiexz@yandex.ru>
2021-03-06 17:28:54 +0300

    Merge from master

--- ruotvet/__init__.py
+++ ruotvet/__init__.py
@@ -5,4 +5,4 @@ if sys.version_info < (3, 7):
 
 from .clients import *
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
--- ruotvet/utils/google.py
+++ ruotvet/utils/google.py
@@ -1,7 +1,7 @@
 from ..http import AIOHTTPClient
 from typing import Optional
 from json import loads
-
+import re
 
 class Google:
     def __init__(self):
@@ -17,15 +17,12 @@ class Google:
         :param proxy: Proxy that will be used during request.
         :return: List of answered questions.
         """
-        url = f"https://cse.google.com/cse/element/v1?rsz=filtered_cse&num={count}&hl={language_code}&source=gcsc" \
-              f"&gss=.com&cselibv=323d4b81541ddb5b&cx=b9d53d8616ca5d2ae&q={query}1&safe=off" \
-              f"&cse_tok=AJvRUv1Q-D5PxjpehBSJ6EnQP5hN:1614378606318&sort=&exp=csqr,cc&oq={query}" \
-              "&gs_l=partner-generic.12...0.0.2.766337.0.0.0.0.0.0.0.0..0.0.csems%2Cnrl%3D13...0.0....34.partner-" \
-              "generic..47.0.0.nm6aVyIGCn4&callback=google.search.cse.api3374"
+        escaped_search_term = query.replace(' ', '+')
+        url = 'https://www.google.com/search?q={}&num={}&hl={}'.format(escaped_search_term, 10,
+                                                                              language_code)
         try:
             response = await self.client.request_text("GET", url, proxy=proxy)
-            if "cursor" in response:
-                response = response.split("google.search.cse.api3374(")[1][:-2]
-                return loads(response)
+            return re.findall(r'(?<=<a href=\"\/url\?esrc=s&amp;q=&amp;rct=j&amp;sa=U&amp;url=)(?:http).{0,500}?(?=\&amp)',str(response),re.MULTILINE)
+
         finally:
             await self.client.close()
